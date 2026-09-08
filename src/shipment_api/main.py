568f71bd410dc8b0f2e39153f68a7739b42dc820
mayurpawar17 <mayurpawar4908@gmail.com>
2026-09-08 23:30:36 +0530

    impl diff request response model

--- src/shipment_api/main.py
+++ src/shipment_api/main.py
@@ -2,7 +2,7 @@ from typing import Any
 
 from fastapi import FastAPI, HTTPException, status
 from .datbase import shipment_data
-from .schemas import Shipment
+from .schemas import Shipment, ShipmentResponse,ShipmentCreate
 from scalar_fastapi import get_scalar_api_reference
 
 app = FastAPI()
@@ -13,22 +13,22 @@ async def root():
     return {"message": "Shipment API is running!"}
 
 
-@app.get("/shipments")
-def get_shipments():
-    return shipment_data
+@app.get("/shipments", response_model=list[ShipmentResponse])
+def get_shipments()->list[ShipmentResponse]:
+    return [ShipmentResponse(**shipment) for shipment in shipment_data]
 
-@app.get("/shipments/{shipment_id}")
-def get_shipment(shipment_id: int) -> dict[str, Any]:
+@app.get("/shipments/{shipment_id}", response_model=ShipmentResponse)
+def get_shipment(shipment_id: int) -> ShipmentResponse:
     for shipment in shipment_data:
         if shipment.shipment_id == shipment_id:
-            return shipment
+            return ShipmentResponse(**shipment)
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Shipment not found")
 
 
-@app.post("/shipments")
-def create_shipment(shipment: Shipment)->dict[str,Any]:
+@app.post("/shipments", response_model=ShipmentResponse)
+def create_shipment(shipment: ShipmentCreate)->ShipmentResponse:
     shipment_data.append(shipment)
-    return shipment.model_dump()
+    return ShipmentResponse(**shipment)
 
 @app.get("/scalar",include_in_schema=False)
 def get_scalar():
--- src/shipment_api/schemas.py
+++ src/shipment_api/schemas.py
@@ -1,7 +1,16 @@
 from pydantic import BaseModel,Field
 
-class Shipment(BaseModel):
+class BaseShipment(BaseModel):
     shipment_id: int = Field(..., gt=0, description="Unique identifier for the shipment")
     content: str = Field(..., max_length=100, description="Description of the shipment content")
     destination: str = Field(..., max_length=100, description="Destination of the shipment")
+
+class Shipment(BaseShipment):
+    status: str = Field(..., max_length=50, description="Current status of the shipment")
+
+
+class ShipmentResponse(BaseShipment):
+    status: str = Field(..., max_length=50, description="Current status of the shipment")
+
+class ShipmentCreate(BaseShipment):
     status: str = Field(..., max_length=50, description="Current status of the shipment")
