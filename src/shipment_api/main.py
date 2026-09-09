b1c4b8b5f9a28332516b8c8e1df98cae0a328309
mayurpkapilit23 <mayurp@kapilit.com>
2026-09-09 17:34:27 +0530

    fix the dict in shipment apis

--- src/shipment_api/main.py
+++ src/shipment_api/main.py
@@ -15,20 +15,20 @@ async def root():
 
 @app.get("/shipments", response_model=list[ShipmentResponse])
 def get_shipments()->list[ShipmentResponse]:
-    return [ShipmentResponse(**shipment) for shipment in shipment_data]
+    return [ShipmentResponse(**shipment.dict()) for shipment in shipment_data]
 
 @app.get("/shipments/{shipment_id}", response_model=ShipmentResponse)
 def get_shipment(shipment_id: int) -> ShipmentResponse:
     for shipment in shipment_data:
         if shipment.shipment_id == shipment_id:
-            return ShipmentResponse(**shipment)
+            return ShipmentResponse(**shipment.dict())
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Shipment not found")
 
 
 @app.post("/shipments", response_model=ShipmentResponse)
 def create_shipment(shipment: ShipmentCreate)->ShipmentResponse:
     shipment_data.append(shipment)
-    return ShipmentResponse(**shipment)
+    return ShipmentResponse(**shipment.dict())
 
 @app.get("/scalar",include_in_schema=False)
 def get_scalar():
