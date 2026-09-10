807db75b6ffc4bed7662db9885904c14be5cc6c1
mayurpkapilit23 <mayurp@kapilit.com>
2026-09-10 17:02:28 +0530

    add sql

--- src/shipment_api/main.py
+++ src/shipment_api/main.py
@@ -9,7 +9,67 @@ from scalar_fastapi import get_scalar_api_reference
 
 app = FastAPI(title="FastAPI with SQLAlchemy + Pydantic + Postgresql")
 
+# Database setup
+engine = create_engine("sqlite:///user.db", connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
 
+
+# Database model
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+
+Base.metadata.create_all(bind=engine)
+
+#pydantic model
+class UserCreate(BaseModel):
+    name: str
+    email: str
+
+class UserResponse(BaseModel):
+    id: int
+    name: str
+    email: str
+
+    class Config:
+        # orm_mode = True
+        from_attributes = True
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+# get_db()
+
+@app.post("/users", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = User(name=user.name, email=user.email)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+@app.get("/users/{user_id}", response_model=UserResponse)
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    if user_id <= 0:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid user ID")
+    if not db.query(User).filter(User.id == user_id).first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    return db.query(User).filter(User.id == user_id).first()
+
+
+@app.get("/users", response_model=List[UserResponse])
+def get_users(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    return db.query(User).offset(skip).limit(limit).all()
+    
 @app.get("/")
 async def root():
     return {"message": "Shipment API is running!"}
@@ -19,3 +79,4 @@ async def root():
 @app.get("/scalar",include_in_schema=False)
 def get_scalar():
     return get_scalar_api_reference(openapi_url=app.openapi_url, title="Shipment API")
+    
